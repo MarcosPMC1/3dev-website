b900317126758729ee9caa3e4be715f12b5baed7
MarcosPMC1 <marcospmc@outlook.com>
2023-05-28 15:41:45 -0300

    ajuste de layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className='scroll-smooth'>
       <head>
         <link rel='shortcut icon' href='/p.png' />
       </head>
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
         <a className='hover:text-white' href="#founders">Fundadores</a>
       </header>
       <section className='p-10 flex flex-col gap-10'>
-        <div className='bg-slate-900 p-10 rounded-lg gap-2 flex flex-col'>
+        <div className='bg-slate-900 p-10 rounded-lg gap-2 flex flex-col mt-10'>
           <h1 className='text-white text-5xl'>Portal do aluno personalizado</h1>
           <span className='text-white text-lg'>Um portal totalmene personalizado de alunos, para sua instituição de ensino. Oferecendo diversas soluções práticas para você</span>d
           <div className='flex flex-row gap-10'>
@@ -27,7 +27,7 @@ export default function Home() {
           </div>
         </div>
         <div className='bg-none p-16' id='modulos'>
-          <h1 className='text-3xl text-white'>Módulos</h1>
+          <h1 className='text-3xl text-white text-center'>Módulos</h1>
           <div className='flex flex-row flex-wrap gap-10 justify-around mt-24'>
             <div className='flex flex-col bg-slate-800 w-[350px] items-center rounded-lg'>
               <img className='rounded-t-lg' src='/chamada.jpg'></img>
@@ -52,7 +52,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='text-white p-16 bg-slate-800 rounded-lg' id='sobre'>
+        <div className='text-white p-16 bg-slate-800 rounded-lg mt-48' id='sobre'>
           <h1 className='text-5xl'>Sobre nós</h1>
           <span className='text-lg'>
             Com o objetivo de fornecer um sistema altamente customizado com diversas funcionalidades
@@ -70,30 +70,30 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='bg-[#CACACA] p-10 rounded-lg' id='founders'>
-          <h1 className='text-slate-700 text-3xl text-center'>Fundadores</h1>
-          <div className='flex flex-row flex-wrap justify-around mt-28'>
-            <div className='w-[200px] h-[350px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
+        <div className='bg-[#CACACA] p-10 rounded-lg mt-48' id='founders'>
+          <h1 className='text-slate-700 text-4xl py-2 text-center'>Fundadores</h1>
+          <div className='flex flex-row flex-wrap justify-around py-16'>
+            <div className='w-[200px] h-[300px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
               <img className='rounded-full w-28' src="/crisp.jpg" alt="" />
-              <h1 className='mt-16'>Filipe Crisp</h1>
+              <h1 className='mt-10'>Filipe Crisp</h1>
               <span>CEO</span>
             </div>
             <div className='bg-black border-black w-[1px]'></div>
-            <div className='w-[200px] h-[350px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
+            <div className='w-[200px] h-[300px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
               <img className='rounded-full w-28' src="https://media.licdn.com/dms/image/D4D03AQEp61YQGpArBA/profile-displayphoto-shrink_800_800/0/1681395566186?e=1690416000&v=beta&t=TE50gjktdoJaqdTEWBkSlIJV0rlfamxyle6LFj3FFcU" alt="" />
-              <h1 className='mt-16'>Marcos Cardoso</h1>
+              <h1 className='mt-10'>Marcos Cardoso</h1>
               <span>CTO</span>
             </div>
             <div className='bg-black border-black w-[1px]'></div>
-            <div className='w-[200px] h-[350px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
+            <div className='w-[200px] h-[300px] bg-slate-700 text-white flex flex-col items-center py-10 rounded-xl'>
               <img className='rounded-full w-28' src="/duzao.jpg" alt="" />
-              <h1 className='mt-16'>Eduardo Gomes</h1>
+              <h1 className='mt-10'>Eduardo Gomes</h1>
               <span>CGO</span>
             </div>
           </div>
         </div>
-        <div className='p-10 bg-slate-900 rounded-2xl' id='contato'>
-          <h1 className='text-white text-2xl px-12'>Entre em contato</h1>
+        <div className='p-10 bg-slate-900 rounded-2xl mt-48' id='contato'>
+          <h1 className='text-yellow-500 py-10 text-3xl px-12'>Entre em contato</h1>
           <div className='flex flex-col text-white px-20 py-2'>
             <label>Nome</label>
             <input type="text" placeholder='Nome' className='px-4 py-2 text-black rounded-lg'/>
